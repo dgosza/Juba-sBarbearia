47e7d15bb4d3004d0227774a25b3e8131f4f702b
Diego Souza <diegomagasouza@gmail.com>
2020-04-25 02:19:26 -0300

    update contato component

--- front/src/components/Main/Contato/index.js
+++ front/src/components/Main/Contato/index.js
@@ -5,24 +5,27 @@ import Formulario from './Formulario'
 //Componente Titulo
 import Titulo from '../../Utils/Titulo'
 
+//Tooltip
+import Tooltip from '@material-ui/core/Tooltip'
+
+import './style.css'
+
 const Contato = () => {
     return (
         <div className="row container">
             <div className="col s12 l12">
                 <Titulo>Contato e Whatsapp</Titulo>
-                <p>Clique no ícone do whatsapp para automaticamente entrar em contato conosco ou se preferir envie um
-                formulário de contato que
-                logo logo responderemos você!</p>
             </div>
 
             <div className="col s12 l7">
                 <Formulario></Formulario>
             </div>
 
-            <div className="col s12 l5">
-                <Whatsapp size={80}></Whatsapp>
+            <div className="col s12 l5 center">
+                <p>Clique no icone abaixo e entre em contato diretamente no whatsapp!</p>
+                <Tooltip title="Entrar em contato no whatsapp"><a href=""><Whatsapp size={80} className="iconWhats"></Whatsapp></a></Tooltip>
             </div>
-
+            <div class="col s12 l12"><br/><br/></div>
         </div>
     )
 }
