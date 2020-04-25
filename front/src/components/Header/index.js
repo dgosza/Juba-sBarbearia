5d05976a53d647cde2cb5f45a051d8a1f0937978
Diego Souza <diegomagasouza@gmail.com>
2020-04-24 23:45:14 -0300

    change header component

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+//Icones
 import { Instagram } from '@styled-icons/boxicons-logos'
-import { Facebook } from '@styled-icons/fa-brands'
+import { SocialFacebookCircular } from '@styled-icons/typicons'
+import { Whatsapp } from '@styled-icons/icomoon'
 
 import ImageLogo from '../../assets/images/jubas.png'
 import './style.css'
+
+//Tooltip
+import Tooltip from '@material-ui/core/Tooltip'
 
 const Header = () => {
 
@@ -15,7 +20,7 @@ const Header = () => {
                     <a href="index.html" className="brand-logo center"> <img src={ImageLogo} alt="Logo Juba's Barbearia" width="120" /> </a>
                     <div className="nav-wrapper">
                         <ul className="left hide-on-med-and-down menu">
-                            <li className="active"><a href="sass.html">Home</a></li>
+                            <li><a href="sass.html">Home</a></li>
                             <li><a href="badges.html">Equipe</a></li>
                             <li><a href="collapsible.html">Serviços</a></li>
                         </ul>
@@ -24,8 +29,19 @@ const Header = () => {
                             <li><a href="sass.html">Produtos</a></li>
                             <li><a href="badges.html">Sobre</a></li>
                             <li><a href="collapsible.html">Contato</a></li>
-                            <li className="icon"><Instagram size={30}></Instagram></li>
-                            <li className="icon"><Facebook size={30}></Facebook></li>
+
+                            <Tooltip title="Juba's no Instagram">
+                                <li className="icon"><a href="https://www.instagram.com/barbearia_jubas/"><Instagram size={30}/></a></li>
+                            </Tooltip>
+
+                            <Tooltip title="Juba's no Facebook">
+                                    <li className="icon"><a href="https://www.facebook.com/barbeariajubas"><SocialFacebookCircular size={35}/></a></li>
+                            </Tooltip>
+
+                            <Tooltip title="Juba's no Whatsapp">
+                                    <li className="icon"><a href=""><Whatsapp size={25}/></a></li>
+                            </Tooltip>
+
                         </ul>
                     </div>
                 </div>
