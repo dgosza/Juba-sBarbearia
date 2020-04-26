058ff26b8ae293f0ff49f9861fa4f1c70a98162d
Diego Souza <diegomagasouza@gmail.com>
2020-04-26 19:13:38 -0300

    logo jubas component

---
+++ front/src/components/Utils/LogoJubas/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Image from '../../../assets/images/jubas.png'
+
+const LogoJubas = ({ dimensao }) =>  <img src={Image} width={dimensao} alt="Logo Juba's" />
+
+export default LogoJubas;
